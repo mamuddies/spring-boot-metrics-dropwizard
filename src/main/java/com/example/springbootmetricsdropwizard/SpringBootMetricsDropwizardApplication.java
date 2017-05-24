6b954d60f8573c3e85634989fb1791a41cd6d321
mamuddies <mamuddies@gmail.com>
2017-05-24 12:46:04 +0530

    Removed unused code

--- src/main/java/com/example/springbootmetricsdropwizard/SpringBootMetricsDropwizardApplication.java
+++ src/main/java/com/example/springbootmetricsdropwizard/SpringBootMetricsDropwizardApplication.java
@@ -3,12 +3,8 @@ package com.example.springbootmetricsdropwizard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
-import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
-
 @SpringBootApplication
-@EnableMetrics(proxyTargetClass = true)
-public class SpringBootMetricsDropwizardApplication extends MetricsConfigurerAdapter {
+public class SpringBootMetricsDropwizardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMetricsDropwizardApplication.class, args);
